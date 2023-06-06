b5ec64dd6548fc7e6b49b049db13f49b71d72f72
nikita_frontend <nikitasar64@gmail.com>
2023-06-06 19:37:26 +0400

    init: add feat read arguments from the command line, add command line helpers

---
+++ services/log.service.js
@@ -0,0 +1,22 @@
+import chalk from 'chalk';
+import dedent from 'dedent-js';
+
+export const printError = (error) => {
+    console.log(chalk.bgRed(' Error ' + ' ' + error));
+}
+
+export const printSuccess = (message) => {
+    console.log(chalk.bgGreen(' Succes ' + ' ' + message))
+}
+
+export const printHelp = () => {
+    console.log(
+        dedent(
+            `${chalk.bgCyan(' Help ')}
+            Без параметров - вывод погоды
+            -s [CITY] для установки города
+            -h для вывода помощи
+            -t [API_KEY] для сохранения токена`
+        )
+    )
+}
---
+++ weather.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env_node
+
+import { getArgs } from "./helpers/args.js";
+import { printHelp } from "./services/log.service.js";
+
+const initCLI = () => {
+    const args = getArgs(process.argv);
+
+    if(args.h){
+        printHelp();
+    }
+}
+
+initCLI();
